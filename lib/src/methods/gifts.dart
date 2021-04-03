a12c0e1c7ecaaefe94297eb85dd0a241651f8709
Waitrum <evstegneych@mail.ru>
2021-04-04 01:06:14 +0700

    add Gifts methods

---
+++ lib/src/methods/gifts.dart
@@ -0,0 +1,22 @@
+import '../api.dart';
+
+class Gifts {
+  final API _api;
+
+  Gifts(this._api);
+
+  /// Returns a list of user gifts.
+  ///
+  /// Params:
+  ///
+  /// `user_id` *(integer)* User ID.
+  ///
+  /// `count` *(integer)* Number of gifts to return.
+  ///
+  /// `offset` *(integer)* Offset needed to return a specific subset of results.
+  Future<dynamic> get([Map<String, dynamic>? params]) async {
+    return _api.request('gifts.get', {
+      ...?params,
+    });
+  }
+}
