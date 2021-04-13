dc7f77b27fe82d358757442deb88d9fb9e3c3c01
Waitrum <evstegneych@mail.ru>
2021-04-13 14:47:00 +0700

    add: new Messages structure

--- lib/src/objects/messages.dart
+++ lib/src/objects/messages.dart
@@ -1,3 +1,48 @@
+import 'package:vklib/src/objects/other/base_responses.dart';
+
+class BaseMessagesMessage extends BaseResponse {
+  BaseMessagesMessage.fromMap(Map<String, dynamic> _map) : super.fromMap(_map) {
+    build();
+  }
+
+  BaseMessagesMessage.fromJson(String _json) : super.fromJson(_json) {
+    build();
+  }
+
+  @override
+  Map<String, dynamic> get body => super.body;
+
+  void build() {
+    date = body['date'];
+    fromId = body['from_id'];
+    id = body['id'];
+    out = body['out'];
+    peerId = body['peer_id'];
+    text = body['text'];
+    conversationMessageId = body['conversation_message_id'];
+
+    for (var item in body['fwd_messages'] ?? []) {
+      fwdMessages!.add(MessagesFwdMessage(item));
+    }
+  }
+
+  int? date;
+
+  int? fromId;
+
+  int? id;
+
+  int? out;
+
+  int? peerId;
+
+  String? text;
+
+  int? conversationMessageId;
+
+  List<MessagesFwdMessage>? fwdMessages;
+}
+
 class MessagesMessage {
   final Map<String, dynamic>? _message;
 
